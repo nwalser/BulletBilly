ac530e179128682e9910b0de8ea0beaea510e290
Nathaniel Walser <personal@nathaniel-walser.com>
2025-04-06 23:17:33 +0200

    added working motor driver

---
+++ microcontroller/BulletBilly/include/PESBoardPinMap.h
@@ -0,0 +1,63 @@
+#ifndef PES_BOARD_PIN_MAP_
+#define PES_BOARD_PIN_MAP_
+
+// PES-Board Pin Names
+#define NEW_PES_BOARD_VERSION
+#ifdef NEW_PES_BOARD_VERSION
+
+    // Servos and Ultrasonic Sensor
+    #define PB_D0 PB_2
+    #define PB_D1 PC_8
+    #define PB_D2 PC_6
+    #define PB_D3 PB_12
+
+    // DC-Motors
+    #define PB_PWM_M1 PB_13
+    #define PB_PWM_M2 PA_9
+    #define PB_PWM_M3 PA_10
+
+    #define PB_ENC_A_M1 PA_6
+    #define PB_ENC_B_M1 PC_7
+    #define PB_ENC_A_M2 PB_6
+    #define PB_ENC_B_M2 PB_7
+    #define PB_ENC_A_M3 PA_0
+    #define PB_ENC_B_M3 PA_1
+
+    #define PB_ENABLE_DCMOTORS PB_15
+
+    // IMU
+    #define PB_IMU_SDA PC_9
+    #define PB_IMU_SCL PA_8
+
+    // SD-Card
+    #define PB_SD_MOSI PC_12
+    #define PB_SD_MISO PC_11
+    #define PB_SD_SCK PC_10
+    #define PB_SD_CS PD_2
+
+#else
+
+    #define PB_D0 PC_9  // ???
+    #define PB_D1 PC_8  // ???
+    #define PB_D2 PC_6  // ???
+    #define PB_D3 PB_12 // ???
+
+    #define PB_PWM_M1 PA_8
+    #define PB_PWM_M2 PA_9
+    #define PB_PWM_M3 PA_13 // ???
+
+    #define PB_ENC_A_M1 PB_6
+    #define PB_ENC_B_M1 PB_7
+    #define PB_ENC_A_M2 PA_6
+    #define PB_ENC_A_M2 PC_7
+    #define PB_ENC_A_M3 PA_1 // ???
+    #define PB_ENC_B_M3 PA_0 // ???
+
+    #define PB_ENABLE_DCMOTORS PB_2 // PB_13 ???
+
+    #define PB_IMU_SDA // ???
+    #define PB_IMU_SCL // ???
+    
+#endif
+
+#endif /* PES_BOARD_PIN_MAP_ */
--- microcontroller/BulletBilly/main.cpp
+++ microcontroller/BulletBilly/main.cpp
@@ -1,62 +1,38 @@
 #include "mbed.h"
 
+
+// pin mappings
+#include "PESBoardPinMap.h"
+
 // imu
 #include "IMU.h"
-
-#define PB_IMU_SDA PC_9
-#define PB_IMU_SCL PA_8
 
 ImuData imu_data;
 IMU imu(PB_IMU_SDA, PB_IMU_SCL);
 
+// motor 1
+#include "DCMotor.h"
 
-// sd card
-#include <SDBlockDevice.h>
-#include <FATFileSystem.h>
+DigitalOut enable_motors(PB_ENABLE_DCMOTORS);
 
-#define PB_SD_MOSI PC_12
-#define PB_SD_MISO PC_11
-#define PB_SD_SCK PC_10
-#define PB_SD_CS PD_2
+const float voltage_max = 12.0f;
+const float gear_ratio = 50.0f;
+const float kn = 200.0f / 12.0f;
+const float counts_per_turn = 64;
+DCMotor motor(PB_PWM_M1, PB_ENC_A_M1, PB_ENC_B_M1, gear_ratio, kn, voltage_max, counts_per_turn);
 
-SDBlockDevice sd(PB_SD_MOSI, PB_SD_MISO, PB_SD_SCK, PB_SD_CS);
-FATFileSystem fs("sd");
-FILE *logFile;
-
+DigitalIn button(BUTTON1);
 
 int main()
 {
-    // mount sd card
-    printf("Initializing SD card...\n");
-    int status = sd.init();
-    if (status != 0) {
-        printf("SD card initialization failed with status %d\n", status);
-        return -1;
-    }
+    while(button) { };
 
-    if (sd.frequency(10000000) != 0) {
-        printf("SDWriter: set frequency failed (not fatal)\n");
-    }
-    if (fs.mount(&sd) != 0) {
-        printf("SDWriter: FS mount failed\n");
-        sd.deinit();
-    }
+    enable_motors = 1;
+    motor.setVelocity(motor.getMaxVelocity() * 0.03f);
+    motor.enableMotionPlanner();
 
-    logFile = fopen("/sd/log.txt", "a");
-    if (logFile == NULL) {
-        printf("Error opening log file.\n");
-        return -1;
-    }
-
-
-    while (true) {
-        imu_data = imu.getImuData();
-        float roll = imu_data.rpy(0);
-        float pitch = imu_data.rpy(1);
-        float yaw = imu_data.rpy(2);
-
-        printf("%.2f, %.2f, %.2f \n", roll, pitch, yaw);
-
-        fprintf(logFile, "%.2f, %.2f, %.2f \n", roll, pitch, yaw);
+    while(true){
+        float rotation = motor.getRotation();
+        printf("%.2f \n", rotation);
     }
 }
